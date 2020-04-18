2cc90c632a735842590cf78d15c5d80e524f236b
Kumar Dastan <kumardastan@gmail.com>
2020-04-18 18:56:01 +0600

    Updates

---
+++ precache-manifest.a54602a9c9ca729f7f3029c8821d0dff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb6e2612dbcdb02b5934278f7e24764",
+    "url": "/sample-react-github-actions/index.html"
+  },
+  {
+    "revision": "99068e59dc49d267b028",
+    "url": "/sample-react-github-actions/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e364baa9856a82042684",
+    "url": "/sample-react-github-actions/static/js/2.dc110b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-react-github-actions/static/js/2.dc110b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99068e59dc49d267b028",
+    "url": "/sample-react-github-actions/static/js/main.80ed4e45.chunk.js"
+  },
+  {
+    "revision": "57e14837e5be1fdbedfb",
+    "url": "/sample-react-github-actions/static/js/runtime-main.a8d04741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-react-github-actions/static/media/logo.5d5d9eef.svg"
+  }
+]);
