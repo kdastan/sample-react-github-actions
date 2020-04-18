2e98c56ddf7316f2767506ea4f9a872b75257727
Kumar Dastan <kumardastan@gmail.com>
2020-04-18 21:31:26 +0600

    feat: add pages with navigation

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Main from "./main";
+import About from "./about";
+
+export { Main, About };
---
+++ src/pages/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Main() {
+  return <Link to="/about">Navigate to about page</Link>;
+}
+
+export default Main;
